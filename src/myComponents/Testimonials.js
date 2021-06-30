9292f7b0e4208474ab89b05ec83b077b8df5c006
om satam <62635289+omsatam@users.noreply.github.com>
2021-06-30 20:40:05 +0530

    Update Testimonials.js

--- src/myComponents/Testimonials.js
+++ src/myComponents/Testimonials.js
@@ -2,18 +2,10 @@ import React from 'react';
 import "./Testimonials.css";
 
 function Testimonials({data,activeComponent}) {
-    // const [activeComponent, setActiveComponent] = useState(null)
-
-    // useEffect(() => {
-
-    //         setActiveComponent(document.getElementsByClassName("active")[0].id)
-    //         console.log(activeComponent)
-
-    //     // },5000)
-    // },[data])
+    
     return (
         <div className="testimonial">
-         {activeComponent &&  <h1>{(data[activeComponent]).message}</h1>}
+         {activeComponent &&  <h1>{(data[activeComponent - 1]).message}</h1>}
         </div>
     )
 }
