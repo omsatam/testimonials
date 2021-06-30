494802a52273cf0ddfedbc0f997e18dddbb2540c
om satam <62635289+omsatam@users.noreply.github.com>
2021-06-30 20:33:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Avatars from "./myComponents/Avatars";
 function App() {
   return (
     <div className="app">
-      <h1>Hey lets build testimonials</h1>
+      
       < Avatars />
     </div>
   );
